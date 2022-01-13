94c70188c1b5cedeeaaab5f468a72b3d458695e0
Nirmal V G <nirmalvg97@gmail.com>
2022-01-13 14:15:02 +0530

    Second commit

--- main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const routes = require('./route.js');
-
-app.listen(4000,()=>{
-    console.log('Server is running on port 4000');
-});
-
-app.use('/',routes);
